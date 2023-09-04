7d3e76b5ac03fe9677d55e07cdd15c61cf1ae4b3
klikki-Lab <klikki.lab@gmail.com>
2023-09-04 20:27:13 +0900

    fix products

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -32,14 +32,14 @@ const Products: NextPageWithLayout = (): JSX.Element => {
 };
 
 Products.getLayout = (page: ReactElement): JSX.Element => {
-    const products = 'products';
+    const products = 'Products';
     return (
         <>
-            <CommonHead title={products} description={`Introducing ${authorName}'s ${products}.`} />
+            <CommonHead title={products} description={`Introducing ${authorName}'s ${products.toLowerCase()}.`} />
 
             <CommonLayout>
                 <TitleHeader props={{
-                    name: products.toLocaleUpperCase(),
+                    name: products.toUpperCase(),
                     path: "M240-160q-33 0-56.5-23.5T160-240q0-33 23.5-56.5T240-320q33 0 56.5 23.5T320-240q0 33-23.5 56.5T240-160Zm240 0q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm240 0q-33 0-56.5-23.5T640-240q0-33 23.5-56.5T720-320q33 0 56.5 23.5T800-240q0 33-23.5 56.5T720-160ZM240-400q-33 0-56.5-23.5T160-480q0-33 23.5-56.5T240-560q33 0 56.5 23.5T320-480q0 33-23.5 56.5T240-400Zm240 0q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm240 0q-33 0-56.5-23.5T640-480q0-33 23.5-56.5T720-560q33 0 56.5 23.5T800-480q0 33-23.5 56.5T720-400ZM240-640q-33 0-56.5-23.5T160-720q0-33 23.5-56.5T240-800q33 0 56.5 23.5T320-720q0 33-23.5 56.5T240-640Zm240 0q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Zm240 0q-33 0-56.5-23.5T640-720q0-33 23.5-56.5T720-800q33 0 56.5 23.5T800-720q0 33-23.5 56.5T720-640Z",
                     color: "currentColor",
                     viewBox: "0 -960 960 960",
